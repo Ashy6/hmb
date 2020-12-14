0bb55ae37e7402b5752339930914c55b4910bb5f
Ashy6 <z1801273437@163.com>
2020-12-14 16:31:02 +0800

    睡眠测评

---
+++ src/main/java/com/hellomybody/hmb/bean/Cp_sleep.java
@@ -0,0 +1,356 @@
+package com.hellomybody.hmb.bean;
+
+public class Cp_sleep {
+    public int id;
+    public String name;
+    public String resource1;
+    public String resource2;
+    public String resource3;
+    public String resource4;
+    public String resource5;
+    public String resource6;
+    public String resource7;
+    public String resource8;
+    public String resource9;
+    public String resource10;
+    public String resource11;
+    public String resource12;
+    public String resource13;
+    public String resource14;
+    public String resource15;
+    public String resource16;
+    public String resource17;
+    public String resource18;
+    public String resource19;
+    public String resource20;
+    public String resource21;
+    public String resource22;
+    public String resource23;
+    public String resource24;
+    public String resource25;
+    public String resource26;
+    public String resource27;
+    public String resource28;
+    public String resource29;
+
+    public Cp_sleep() {
+    }
+
+    public Cp_sleep(String name, String resource1, String resource2, String resource3, String resource4, String resource5, String resource6, String resource7, String resource8, String resource9, String resource10, String resource11, String resource12, String resource13, String resource14, String resource15, String resource16, String resource17, String resource18, String resource19, String resource20, String resource21, String resource22, String resource23, String resource24, String resource25, String resource26, String resource27, String resource28, String resource29) {
+        this.name = name;
+        this.resource1 = resource1;
+        this.resource2 = resource2;
+        this.resource3 = resource3;
+        this.resource4 = resource4;
+        this.resource5 = resource5;
+        this.resource6 = resource6;
+        this.resource7 = resource7;
+        this.resource8 = resource8;
+        this.resource9 = resource9;
+        this.resource10 = resource10;
+        this.resource11 = resource11;
+        this.resource12 = resource12;
+        this.resource13 = resource13;
+        this.resource14 = resource14;
+        this.resource15 = resource15;
+        this.resource16 = resource16;
+        this.resource17 = resource17;
+        this.resource18 = resource18;
+        this.resource19 = resource19;
+        this.resource20 = resource20;
+        this.resource21 = resource21;
+        this.resource22 = resource22;
+        this.resource23 = resource23;
+        this.resource24 = resource24;
+        this.resource25 = resource25;
+        this.resource26 = resource26;
+        this.resource27 = resource27;
+        this.resource28 = resource28;
+        this.resource29 = resource29;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getResource1() {
+        return resource1;
+    }
+
+    public void setResource1(String resource1) {
+        this.resource1 = resource1;
+    }
+
+    public String getResource2() {
+        return resource2;
+    }
+
+    public void setResource2(String resource2) {
+        this.resource2 = resource2;
+    }
+
+    public String getResource3() {
+        return resource3;
+    }
+
+    public void setResource3(String resource3) {
+        this.resource3 = resource3;
+    }
+
+    public String getResource4() {
+        return resource4;
+    }
+
+    public void setResource4(String resource4) {
+        this.resource4 = resource4;
+    }
+
+    public String getResource5() {
+        return resource5;
+    }
+
+    public void setResource5(String resource5) {
+        this.resource5 = resource5;
+    }
+
+    public String getResource6() {
+        return resource6;
+    }
+
+    public void setResource6(String resource6) {
+        this.resource6 = resource6;
+    }
+
+    public String getResource7() {
+        return resource7;
+    }
+
+    public void setResource7(String resource7) {
+        this.resource7 = resource7;
+    }
+
+    public String getResource8() {
+        return resource8;
+    }
+
+    public void setResource8(String resource8) {
+        this.resource8 = resource8;
+    }
+
+    public String getResource9() {
+        return resource9;
+    }
+
+    public void setResource9(String resource9) {
+        this.resource9 = resource9;
+    }
+
+    public String getResource10() {
+        return resource10;
+    }
+
+    public void setResource10(String resource10) {
+        this.resource10 = resource10;
+    }
+
+    public String getResource11() {
+        return resource11;
+    }
+
+    public void setResource11(String resource11) {
+        this.resource11 = resource11;
+    }
+
+    public String getResource12() {
+        return resource12;
+    }
+
+    public void setResource12(String resource12) {
+        this.resource12 = resource12;
+    }
+
+    public String getResource13() {
+        return resource13;
+    }
+
+    public void setResource13(String resource13) {
+        this.resource13 = resource13;
+    }
+
+    public String getResource14() {
+        return resource14;
+    }
+
+    public void setResource14(String resource14) {
+        this.resource14 = resource14;
+    }
+
+    public String getResource15() {
+        return resource15;
+    }
+
+    public void setResource15(String resource15) {
+        this.resource15 = resource15;
+    }
+
+    public String getResource16() {
+        return resource16;
+    }
+
+    public void setResource16(String resource16) {
+        this.resource16 = resource16;
+    }
+
+    public String getResource17() {
+        return resource17;
+    }
+
+    public void setResource17(String resource17) {
+        this.resource17 = resource17;
+    }
+
+    public String getResource18() {
+        return resource18;
+    }
+
+    public void setResource18(String resource18) {
+        this.resource18 = resource18;
+    }
+
+    public String getResource19() {
+        return resource19;
+    }
+
+    public void setResource19(String resource19) {
+        this.resource19 = resource19;
+    }
+
+    public String getResource20() {
+        return resource20;
+    }
+
+    public void setResource20(String resource20) {
+        this.resource20 = resource20;
+    }
+
+    public String getResource21() {
+        return resource21;
+    }
+
+    public void setResource21(String resource21) {
+        this.resource21 = resource21;
+    }
+
+    public String getResource22() {
+        return resource22;
+    }
+
+    public void setResource22(String resource22) {
+        this.resource22 = resource22;
+    }
+
+    public String getResource23() {
+        return resource23;
+    }
+
+    public void setResource23(String resource23) {
+        this.resource23 = resource23;
+    }
+
+    public String getResource24() {
+        return resource24;
+    }
+
+    public void setResource24(String resource24) {
+        this.resource24 = resource24;
+    }
+
+    public String getResource25() {
+        return resource25;
+    }
+
+    public void setResource25(String resource25) {
+        this.resource25 = resource25;
+    }
+
+    public String getResource26() {
+        return resource26;
+    }
+
+    public void setResource26(String resource26) {
+        this.resource26 = resource26;
+    }
+
+    public String getResource27() {
+        return resource27;
+    }
+
+    public void setResource27(String resource27) {
+        this.resource27 = resource27;
+    }
+
+    public String getResource28() {
+        return resource28;
+    }
+
+    public void setResource28(String resource28) {
+        this.resource28 = resource28;
+    }
+
+    public String getResource29() {
+        return resource29;
+    }
+
+    public void setResource29(String resource29) {
+        this.resource29 = resource29;
+    }
+
+    @Override
+    public String toString() {
+        return "Cp_sleep{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", resource1='" + resource1 + '\'' +
+                ", resource2='" + resource2 + '\'' +
+                ", resource3='" + resource3 + '\'' +
+                ", resource4='" + resource4 + '\'' +
+                ", resource5='" + resource5 + '\'' +
+                ", resource6='" + resource6 + '\'' +
+                ", resource7='" + resource7 + '\'' +
+                ", resource8='" + resource8 + '\'' +
+                ", resource9='" + resource9 + '\'' +
+                ", resource10='" + resource10 + '\'' +
+                ", resource11='" + resource11 + '\'' +
+                ", resource12='" + resource12 + '\'' +
+                ", resource13='" + resource13 + '\'' +
+                ", resource14='" + resource14 + '\'' +
+                ", resource15='" + resource15 + '\'' +
+                ", resource16='" + resource16 + '\'' +
+                ", resource17='" + resource17 + '\'' +
+                ", resource18='" + resource18 + '\'' +
+                ", resource19='" + resource19 + '\'' +
+                ", resource20='" + resource20 + '\'' +
+                ", resource21='" + resource21 + '\'' +
+                ", resource22='" + resource22 + '\'' +
+                ", resource23='" + resource23 + '\'' +
+                ", resource24='" + resource24 + '\'' +
+                ", resource25='" + resource25 + '\'' +
+                ", resource26='" + resource26 + '\'' +
+                ", resource27='" + resource27 + '\'' +
+                ", resource28='" + resource28 + '\'' +
+                ", resource29='" + resource29 + '\'' +
+                '}';
+    }
+}
