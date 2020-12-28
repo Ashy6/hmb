0996e5e3d977d5972544ac6e085a285469b4f144
Ashy6 <z1801273437@163.com>
2020-12-29 06:01:45 +0800

    提交

---
+++ src/main/java/com/hellomybody/hmb/controller/asas.java
@@ -0,0 +1,16 @@
+package com.hellomybody.hmb.controller;
+
+import java.util.Arrays;
+
+public class asas {
+    public static void main(String[] args) {
+
+        String a1 = "123.sdggs.1234df";
+        String[] a2 = a1.split("\\.");
+        String a3 = Arrays.toString(a2);
+        System.out.println(Arrays.toString(a2));
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
